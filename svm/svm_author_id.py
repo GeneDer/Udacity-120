e5da0ce21b717e32a4e5ae0c2228bd7194421720
Gene Der Su <e870252314@yahoo.com.tw>
2016-05-24 14:14:28 -0700

    finished mini project 2.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+#print len(features_train),len(labels_test)
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(C = 10000, kernel = 'rbf')
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
+print sum(pred)
 #########################################################
 
 
