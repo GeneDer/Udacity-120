a961e45ddf42cf292f2a3ae6dbb2b50eb7027336
Gene Der Su <e870252314@yahoo.com.tw>
2016-05-24 15:13:58 -0700

    finish mini project 3.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,11 +20,14 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print len(features_train[0])
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+print clf.score(features_test,labels_test)
 
 #########################################################
 
