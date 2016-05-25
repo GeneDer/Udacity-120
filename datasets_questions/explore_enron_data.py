b5536e25d0ea9d0ce97e98f74dfc2869304d8ca5
Gene Der Su <e870252314@yahoo.com.tw>
2016-05-24 18:00:08 -0700

    done with dataset questions.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,37 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
 
+print len(enron_data.values()[0])
+
+print sum([enron_data[i]["poi"] for i in enron_data])
+
+print enron_data['PRENTICE JAMES']['total_stock_value']
+
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+lay = enron_data['LAY KENNETH L']['total_payments']
+skilling = enron_data['SKILLING JEFFREY K']['total_payments']
+fastow = enron_data['FASTOW ANDREW S']['total_payments']
+if lay > skilling and lay > fastow:
+    print 'Lay: %s'%lay
+elif skilling > lay and skilling > fastow:
+    print 'Skilling: %s'%skilling
+else:
+    print 'Fastow: %s'%fastow
+
+countSalary = 0
+countEmail = 0
+countTotalPayment = 0
+for i in enron_data:
+    if enron_data[i]['salary'] != 'NaN':
+        countSalary += 1
+    if enron_data[i]['email_address'] != 'NaN':
+        countEmail += 1
+    if enron_data[i]['total_payments'] == 'NaN':
+        countTotalPayment += 1
+print countSalary, countEmail
+print countTotalPayment, float(countTotalPayment)/len(enron_data)*100
