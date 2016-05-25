21a322898f49f50899b75b7fd8176177797c01a8
Gene Der Su <e870252314@yahoo.com.tw>
2016-05-24 21:40:37 -0700

    k-means mini project completed

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,13 +59,32 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+maxESO = finance_features[0][2]
+minESO = finance_features[0][2]
+maxS = finance_features[0][1]
+minS = finance_features[1][1]
+for f1, f2, _ in finance_features:
+    plt.scatter( f1, f2)
+    if f2 != 0:
+        if f2 > maxESO:
+            maxESO = f2
+        if f2 < minESO:
+            minESO = f2
+    if f1 != 0:
+        if f1 > maxS:
+            maxS = f1
+        if f1 < minS:
+            minS = f1
 plt.show()
+print minESO, maxESO
+print minS, maxS
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+clu = KMeans(n_clusters = 2)
+clu.fit(finance_features)
+pred = clu.predict(finance_features)
 
 
 
